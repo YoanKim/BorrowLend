db246b488851afb829bc12b17dfcce50575d6629
YoanKim <93916033+YoanKim@users.noreply.github.com>
2021-11-09 12:04:58 +0200

    Прикачвам всички cs-ове на задачата

---
+++ ExtenseType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BorrowLend.Models
+{
+    public class ExtenseType
+    {
+        private int id;
+        private string extenseTypeText;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string ExtenseTypeText
+        {
+            get { return extenseTypeText; }
+            set { extenseTypeText = value; }
+        }
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BorrowLend.Models
+{
+    public class Item
+    {
+        private int id;
+        private string itemName;
+        private string borrower;
+        private string lender;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string ItemName
+        {
+            get { return itemName; }
+            set { itemName = value; }
+        }
+
+        public string Borrower
+        {
+            get { return borrower; }
+            set { borrower = value; }
+        }
+
+        public string Lender
+        {
+            get { return lender; }
+            set { lender = value; }
+        }
+    }
+}
